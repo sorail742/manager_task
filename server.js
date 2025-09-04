c64bcbe2e49e7f1c6e008bc823d8dfdb671e4e2c
Your Name <keithsorail@gmail.com>
2025-09-04 18:58:20 +0000

    dernieres modification de cors

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://ton-frontend.netlify.app"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  origin: 'https://task-manager-frontend-d23r.onrender.com',
+  credentials: true
 }));
+
 
 app.use(express.json());
 
