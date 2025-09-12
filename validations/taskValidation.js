4a79db2eb4db727180bb0ca8b842a169d3806f0a
Your Name <keithsorail@gmail.com>
2025-09-12 12:36:40 +0000

    status corrigé

--- validations/taskValidation.js
+++ validations/taskValidation.js
@@ -4,6 +4,5 @@ module.exports = Joi.object({
   title: Joi.string().min(3).max(200).required(),
   description: Joi.string().allow('', null),
   priority: Joi.string().valid('low','medium','high').optional(),
-  status: Joi.string().optional(),
   dueDate: Joi.date().optional()
 });
